1be04adf4c8381e01554b012747ee50005597c7a
Amr Gharseldin <gharseldin@aucegypt.edu>
2020-07-01 02:44:53 -0700

    Update 1370_inc_dec.py
    Greate Job my friend

--- leetcode/1370_inc_dec.py
+++ leetcode/1370_inc_dec.py
@@ -1,4 +1,6 @@
     def sortString(self, s: str) -> str:
+        ### Why are we searching for Max? There is no need. 
+        ### The problem constraints say that all letters are small
         str = list(s)
         maxx = str[0]
         # print(ord(maxx))
@@ -6,15 +8,32 @@
             # print(i,maxx)
             if i > maxx:
                 maxx = i
+        ### Max is not even being used. 
         maxx = ord(maxx) -  ord('a')
         countList = [0] * 26
         for i in str:
             idx = ord(i) - ord('a')
             countList[idx]+=1
         final_list=[]
+        
+        """ The Condition on this while loop is a dangerous one
+            Calculating the sum of a list takes O(n) under the hood 
+            The while loop loops n times, so total is O(n2)
+            you can say The following
+                while len(final_list) < len(s)
+           
+        """
         while sum(countList) >0:
             # print(countList)
             for i in range(len(countList)):
+                """
+                    When you get an if else statement like this write it as follows
+                    if countList !=0:
+                        final_list.append(chr(i+97))
+                        countList[i]-=1
+                    
+                    So you don't need an else or a continue
+               """     
                 if countList[i] ==0:
                     continue
                 else:
